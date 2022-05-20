d3a2e0f623825a46896a5a73147b8db86e6fb601
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-05-20 17:18:12 +0300

    Declare Participant class and initialize its features in initializer

--- Assignment1_Furkan_Açıkgöz.playground/Contents.swift
+++ Assignment1_Furkan_Açıkgöz.playground/Contents.swift
@@ -11,3 +11,23 @@ enum Name {
     case Todeb
     case Enuygun
 }
+
+class Participant {
+    private var name: String?
+    private var surname: String?
+    private var email: String?
+    private var isSelected: Bool?
+    private var nameOfBootcamp: Name?
+    private var codeOfBootcamp: Int?
+    
+    init(name: String, surname: String, email:String,
+         isSelected: Bool, nameOfBootcamp: Name,
+         codeOfBootcamp: Int) {
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.isSelected = isSelected
+        self.nameOfBootcamp = nameOfBootcamp
+        self.codeOfBootcamp = codeOfBootcamp
+    }
+}
